808c5b2ab41f4478c39d4dfb172d3e6542aa37cf
Justin <juc81@pitt.edu>
2022-08-05 15:41:09 -0400

    added helpers in handlebars for formatDate

--- app.js
+++ app.js
@@ -27,8 +27,19 @@ app.use(express.json())
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
+//Handlebar Helpers
+const { formatDate } = require('./helpers/hbs')
+
 //Handlebars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('.hbs', exphbs.engine({ 
+    helpers: {
+        formatDate,
+    }, 
+    defaultLayout: 'main', 
+    extname: '.hbs' 
+})
+)
 app.set('view engine', '.hbs')
 
 //Sessions middleware
