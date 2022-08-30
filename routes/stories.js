bfcd3ea44ca5352d1262947d12e0bf398734713f
Justin <juc81@pitt.edu>
2022-08-29 23:33:27 -0400

    changed story to lowercase in routes

--- routes/stories.js
+++ routes/stories.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { ensureAuth } = require('../middleware/auth')
-const Story = require('../models/Story')
+const Story = require('../models/story')
 
 // @desc Show add page
 // @route GET /stories/add
