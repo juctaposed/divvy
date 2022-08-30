5230933fe7fe5105408d8645eb963449a0b34d87
Justin <juc81@pitt.edu>
2022-08-29 23:17:56 -0400

    make it better

--- config/passport.js
+++ config/passport.js
@@ -29,7 +29,7 @@ module.exports = function (passport) {
                 done(null, user)
             }
         } catch (error) {
-            console.error(err)
+            console.log(error)
         }
         console.log(profile)
     }))
